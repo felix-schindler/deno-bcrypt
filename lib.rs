e54ea3737cac5d05c50c126785f3dbca1eca9d90
Felix <felix-schindler@outlook.com>
2023-08-04 11:02:15 +0200

    lib.rs: Ignore not_unsafe_ptr_arg_deref

--- lib.rs
+++ lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+
 extern crate bcrypt;
 
 use deno_bindgen::deno_bindgen;
@@ -5,16 +7,11 @@ use bcrypt::{DEFAULT_COST, hash as bcrypt_hash, verify as bcrypt_verify};
 
 #[deno_bindgen(non_blocking)]
 fn hash(password: &str) -> String {
-	return bcrypt_hash(password, DEFAULT_COST).unwrap();
+	bcrypt_hash(password, DEFAULT_COST).unwrap()
 }
 
 #[deno_bindgen(non_blocking)]
 fn verify(password: &str, hash: &str) -> i8 {
 	let result = bcrypt_verify(password, hash).unwrap();
-
-	if result {
-		return 1;
-	} else {
-		return 0;
-	}
+	result as i8
 }
