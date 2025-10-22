af9cc640b04cfb0005ec065c4c9aba07c4021afe
Felix <felix-schindler@outlook.com>
2025-10-22 21:43:14 +0200

    fix(bench): update to fix errors after deno v2.5

--- all.bench.ts
+++ all.bench.ts
@@ -1,8 +1,5 @@
 import { hash, verify } from "./mod.ts";
-import {
-	compare as verify2,
-	hash as hash2,
-} from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+import { compare as verify2, hash as hash2 } from "bcrypt";
 
 const password =
 	"2gnF!WAcyhp#kB@tcYQa2$A%P64jEmXY!@8n2GSH$GggfgGfP*qH!EWwDaB%5mdB6pW2fK!KD@YNjvqwREfRCCAPc54c5@Sk";
